0efdca774066232cd9741913b7bcbb4d88fab44e
Mike Ponton <pontonmikecgi@gmail.com>
2024-04-13 21:37:57 +0200

    [Database] Fetching Cards only when displaying them

--- src/components/Database/CardList.vue
+++ src/components/Database/CardList.vue
@@ -1,31 +1,13 @@
 <template>
   <div>
-    <div>
-      <div v-if="totalPages > 1">
-        <button v-if="currentPage > 1" @click="prevPage">Previous</button>
-        <span>Page {{ currentPage }} of {{ totalPages }}</span>
-        <button v-if="currentPage < totalPages" @click="nextPage">Next</button>
-      </div>
+    <div v-if="totalPages > 1">
+      <button v-if="currentPage > 1" @click="prevPage">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button v-if="currentPage < totalPages" @click="nextPage">Next</button>
     </div>
 
-    <div v-for="card in paginatedCards" :key="card.cardId">
+    <div v-for="card in cards" :key="card.cardId">
       <CardInfo :card="card" />
-    </div>
-
-    <div>
-      <div v-if="totalPages > 1">
-        <button v-if="currentPage > 1" @click="prevPage">Previous</button>
-        <span>Page {{ currentPage }} of {{ totalPages }}</span>
-        <button v-if="currentPage < totalPages" @click="nextPage">Next</button>
-      </div>
-    </div>
-
-    <div>
-      <div>
-        <select v-model="perPage" @change="pageOptionsChanged">
-          <option v-for="option in perPageOptions" :key="option" :value="option">{{ option }}</option>
-        </select>
-      </div>
     </div>
   </div>
 </template>
@@ -41,48 +23,52 @@ export default {
     return {
       cards: [],
       currentPage: 1,
-      perPage: 25,
-      perPageOptions: [10, 25, 50, 100],
+      perPage: 10,
       totalPages: 1,
     };
   },
   created() {
     this.fetchCards();
   },
+  watch: {
+    currentPage: {
+      handler() {
+        this.fetchCards();
+      },
+      immediate: true
+    },
+    perPage: {
+      handler() {
+        this.fetchCards();
+      },
+      immediate: true
+    }
+  },
   methods: {
     async fetchCards() {
       try {
-        const url = "https://localhost:7241/cards";
+        const url = `https://localhost:7241/cards/page=${this.currentPage}&perPage=${this.perPage}`;
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
-        this.cards = data;
-        this.totalPages = Math.ceil(this.cards.length / this.perPage);
+        this.cards = data.cards;
+        this.totalPages = Math.ceil(data.totalCards / this.perPage);
       } catch (error) {
         console.error(error);
       }
     },
     prevPage() {
-      this.currentPage = Math.max(1, this.currentPage - 1); // Ensure it doesn't go below 1
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
     },
     nextPage() {
-      this.currentPage = Math.min(this.totalPages, this.currentPage + 1); // Ensure it doesn't go above totalPages
-    },
-    pageOptionsChanged() {
-      this.perPage = event.target.value;
-      this.totalPages = Math.ceil(this.cards.length / this.perPage);
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
     }
-  },
-  computed: {
-    paginatedCards() {
-      const startIndex = (this.currentPage - 1) * this.perPage;
-      const endIndex = Math.min(startIndex + this.perPage, this.cards.length);
-      return this.cards.slice(startIndex, endIndex);
-    },
   },
 };
 </script>
 
 <style scoped>
-/* Your component styles */
 </style>
